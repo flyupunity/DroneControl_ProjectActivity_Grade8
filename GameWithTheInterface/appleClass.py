3af4cb91e6e7f64dc776e1ec436a9e50b7020d3a
flyupunity <flyupworking@gmail.com>
2025-02-15 18:36:45 +0300

    It doesn't work

--- GameWithTheInterface/appleClass.py
+++ GameWithTheInterface/appleClass.py
@@ -12,13 +12,12 @@ class Apple(object):
     def __init__(self, startPos_x, startPos_y):
         super().__init__()
 
-        self.x = startPos_x
-        self.y = startPos_y
-
         self.image = pygame.image.load('UI\\apple0.1_0.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.image.get_width()/2, self.image.get_height()/2))
 
         self.image_rect = self.image.get_rect()
+        self.image_rect.x = startPos_x
+        self.image_rect.y = startPos_y
 
         self.isDragged = False
 
@@ -28,24 +27,22 @@ class Apple(object):
     def draw(self, screen):
 
         self.image_rect = self.image.get_rect()
-        print(self.image.get_rect())
+        print(self.image_rect.x, self.image_rect.y)
 
         if self.isDragged:
             mouse_x, mouse_y = pygame.mouse.get_pos()
 
             if self.deviation_x == 0 or self.deviation_y == 0:
-                self.deviation_x = mouse_x - self.x
-                self.deviation_y = mouse_y - self.y
+                self.deviation_x = mouse_x - self.image_rect.x
+                self.deviation_y = mouse_y - self.image_rect.y
             
-            self.x, self.y = mouse_x - self.deviation_x, mouse_y - self.deviation_y
+            self.image_rect.x, self.image_rect.y = mouse_x - self.deviation_x, mouse_y - self.deviation_y
             
         else:
             self.deviation_x = 0
             self.deviation_y = 0
 
-        self.image.get_rect().x = self.x
-        self.image.get_rect().y = self.x
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, (self.image_rect.x, self.image_rect.y))
 
     def CheckingTheDragging(self, isDragged):
         self.isDragged = isDragged
