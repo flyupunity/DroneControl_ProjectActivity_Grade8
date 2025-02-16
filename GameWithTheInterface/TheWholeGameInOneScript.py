3aedb18cf8cdf45b2d3441280b4ac836819fe023
flyupunity <flyupworking@gmail.com>
2025-02-16 14:48:52 +0300

    Saveopoint
    A save point where part of the game is placed in the file "TheWholeGameInOneScript.py" (later it is planned to place the entire game in this file).

--- GameWithTheInterface/TheWholeGameInOneScript.py
+++ GameWithTheInterface/TheWholeGameInOneScript.py
@@ -5,15 +5,174 @@ import pickle
 import cv2
 import pygame
 import sys
-import appleClass
+#import appleClass
 #import followingTheHand
 import random
 
+class Apple(object):
+    def __init__(self, startPos_x, startPos_y):
+        #super().__init__()
+
+        self.x = startPos_x
+        self.y = startPos_y
+
+        self.image = pygame.image.load('UI\\apple0.1_0.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()//2, self.image.get_height()//2))
+
+        self.image_rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.isDragged = False
+
+        self.deviation_x = 0
+        self.deviation_y = 0
+
+        #screen = pygame.display.get_surface()
+        #self.area = screen.get_rect()
+
+    def draw(self, screen):
+
+        if self.isDragged:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            
+            self.x = mouse_x - self.deviation_x
+            self.y = mouse_y - self.deviation_y
+
+
+        self.image_rect.topleft = (self.x, self.y)
+        screen.blit(self.image, (self.x, self.y))
+
+    def CheckingTheDragging(self, isDragged):
+        self.isDragged = isDragged
+        if isDragged:
+            x, y = pygame.mouse.get_pos()
+            self.deviation_x = x - self.x
+            self.deviation_y = y - self.y
+        else:
+            self.deviation_x = 0
+            self.deviation_y = 0
 
 def main():
     print('All libraries are imported!')
     print('Please, waiting...')
     pygame.init()
+
+
+    #window_size = (800, 600)
+    width, height = pygame.display.set_mode().get_size()
+    window_size = (width, height)
+
+    #screen = pygame.display.set_mode(window_size)
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    pygame.display.set_caption("User Interface")
+
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+
+
+
+
+    font = pygame.font.SysFont(None, 100)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if exit_button.collidepoint(event.pos):
+                    pygame.quit()
+                    sys.exit()
+                elif start_button.collidepoint(event.pos):
+                    running = False
+
+
+
+
+
+        screen.fill(WHITE)
+
+        #Background
+        startBackground = pygame.image.load('UI\\startBackground_0.png').convert_alpha()
+        startBackground = pygame.transform.scale(startBackground, (width, height))
+        screen.blit(startBackground, (0, 0))
+
+
+
+        #Start button
+        #start_button = pygame.Rect(width//2, height//2 - (50)/2, 200, 50)
+        start_button = pygame.Rect(0, 0, 400, 100)
+        start_button.center = (width // 2, height // 2 - (100)/1.5 + 200)
+        pygame.draw.rect(screen, RED, start_button, 0, 20)
+        
+        #Start text
+        start_text = font.render("Start", True, WHITE)
+
+        start_text_rect = start_text.get_rect()
+        start_text_rect.center = (start_button.x + start_button.w // 2, start_button.y + start_button.h // 2)
+        screen.blit(start_text, start_text_rect.topleft)
+
+
+
+
+        #Exit button
+        exit_button = pygame.Rect(0, 0, 400, 100)
+        exit_button.center = (width // 2, height // 2 + (100)/1.5 + 200)
+        pygame.draw.rect(screen, RED, exit_button, 0, 20)
+
+        #Exit text
+        exit_text = font.render("Exit", True, WHITE)
+
+        exit_text_rect = exit_text.get_rect()
+        exit_text_rect.center = (exit_button.x + exit_button.w // 2, exit_button.y + exit_button.h // 2)
+        screen.blit(exit_text, exit_text_rect.topleft)
+
+
+
+        #Name text - https://www.pygame.org/pcr/hollow_outline/index.php
+
+        font = pygame.font.SysFont(None, 100)
+        NAME_TEXT = (0, 0, 0)
+
+
+        name_text = font.render(f"CONTROL OF THE MOUSE COURSE", True, NAME_TEXT)
+        name_text_rect = name_text.get_rect()
+        name_text_rect.center = (width // 2, 150)
+        screen.blit(name_text, name_text_rect.topleft)
+
+        name_text = font.render(f"BY HAND GESTURES", True, NAME_TEXT)
+        name_text_rect = name_text.get_rect()
+        name_text_rect.center = (width // 2, 250)
+        screen.blit(name_text, name_text_rect.topleft)
+
+        name_text = font.render(f"USING NEURAL NETWORKS", True, NAME_TEXT)
+        name_text_rect = name_text.get_rect()
+        name_text_rect.center = (width // 2, 350)
+        screen.blit(name_text, name_text_rect.topleft)
+
+        name_text = font.render(f"VIA USER INTERFACE", True, NAME_TEXT)
+        name_text_rect = name_text.get_rect()
+        name_text_rect.center = (width // 2, 450)
+        screen.blit(name_text, name_text_rect.topleft)
+        
+        #screen.blit(name_text, (width // 2, 300))
+
+        #name_text_rect = name_text.get_rect()
+        #name_text_rect.center = (0, name_button.y + name_button.h // 2)
+        #screen.blit(name_text, (width // 2, ))
+
+
+
+
+        #Display update
+        pygame.display.update()
+        #pygame.display.flip()
+
+
+
+
+
+
 
 
 
@@ -45,21 +204,18 @@ def main():
         print('Please, check the camera, maybe another app is using the camera')
 
 
-    #window_size = (800, 600)
-    width, height = pygame.display.set_mode().get_size()
-    window_size = (width, height)
 
-    #screen = pygame.display.set_mode(window_size)
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-    pygame.display.set_caption("User Interface")
 
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
+
+
+
+
+
+
 
     font = pygame.font.SysFont(None, 55)
-    applesFromClass = [appleClass.Apple(random.randrange(1341, 1446), random.randrange(701, 729)) for _ in range(5)]
+    applesFromClass = [Apple(random.randrange(1341, 1446), random.randrange(701, 729)) for _ in range(5)]
 
 
     running = True
@@ -176,6 +332,7 @@ def main():
                 prediction = model.predict([np.asarray(data_aux)])
 
                 print(prediction[0])
+                
                 if int(prediction[0]) == 0 or int(prediction[0]) == 1:
                     
                     width_hand, height_hand, color = img.shape
@@ -190,14 +347,21 @@ def main():
                     if cursor_height < 0: cursor_height = 0
                     if cursor_height > screen_height: cursor_height = screen_height
                     
-                    pyautogui.moveTo(cursor_width, cursor_height)
+
                     position = (cursor_width, cursor_height)
 
+                    if int(prediction[0]) == 0:
+                        pyautogui.moveTo(cursor_width, cursor_height)
                     if int(prediction[0]) == 1:
-                        clickVer += 1
-                        if clickVer >= 10:
-                            pyautogui.click()
-                            clickVer = 0
+                        pyautogui.mouseDown()
+                        pyautogui.moveTo(cursor_width, cursor_height)
+                        
+                        
+                        #pyautogui.dragTo(cursor_width, cursor_height, button='left')
+                        #clickVer += 1
+                        #if clickVer >= 10:
+                        #    pyautogui.click()
+                        #    clickVer = 0
                         
                 if int(prediction[0]) != 1:
                     clickVer = 0
